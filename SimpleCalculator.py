5bebcaf87fea2d5ccc4df313e4536c6773bb6713
IT24103479 <it24103479@my.sliit.lk>
2026-01-19 12:12:35 +0530

    implemented histor list

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -1,110 +1,127 @@
-#TODO: Write the functions for arithmatic operations here
-#These functions should cover Task 2
+history = []
 
 def add(a,b):
     return a+b
-    
-def sub(a,b):
+
+def subtract(a,b):
     return a-b
 
-def mul(a,b):
+def multiply(a,b):
     return a*b
 
-def div(a,b):
-    return a/b
-    
+def divide(a,b):
+    try:
+        return a/b
+    except Exception as e:
+        print(e)
+
 def power(a,b):
     return a**b
-    
-def rem(a,b):
+
+def remainder(a,b):
     return a%b
-#-------------------------------------
-#TODO: Write the select_op(choice) function here
-#This function sould cover Task 1 (Section 2) and Task 3
+
 
 def select_op(choice):
-    
-    if choice =='#':
-         return -1
-    if choice == '$':
-         return  True
-       
-    if choice not in ['+','-','*','/','^','%']:
-       print("Unrecognized operation")
-       return True
-       
-    
-    
-    first = input("Enter first number: ")
-    if first in ['$','#']:
-        if first == '#':
-            return -1
-        return True
-        
-    try:
-        a = float(first)
-    except:
-        print("Not a valid number,please enter again")
-        return True
+    if (choice == '#'):
+        return -1
 
-  
-    second = input("Enter second number: ")
-    if second in ['$','#']:
-        if second == '#':
-            return -1
-        return True
-    try:
-        b = float(second)
-    except:
-        print("Not a valid number,please enter again")
-        return True
+    elif (choice == '$'):
+        return 0
 
-           
-           
-        
-        
-    try:
+    elif (choice == '?'):
+        if not history:
+            print("No past calculations to show")
+        else:
+            for item in history:
+                print(item)
+        return 0
+
+    elif (choice in ('+','-','*','/','^','%')):
+        while (True):
+            num1s = str(input("Enter first number: "))
+            print(num1s)
+            if num1s.endswith('$'):
+                return 0
+            if num1s.endswith('#'):
+                return -1
+
+            try:
+                num1 = float(num1s)
+                break
+            except:
+                print("Not a valid number,please enter again")
+                continue
+
+        while (True):
+            num2s = str(input("Enter second number: "))
+            print(num2s)
+            if num2s.endswith('$'):
+                return 0
+            if num2s.endswith('#'):
+                return -1
+
+            try:
+                num2 = float(num2s)
+                break
+            except:
+                print("Not a valid number,please enter again")
+                continue
+
+        result = 0.0
+
         if choice == '+':
-            print(add(a, b))
+            result = add(num1, num2)
+            operation = f"{num1} + {num2} = {result}"
+
         elif choice == '-':
-            print(sub(a, b))
+            result = subtract(num1, num2)
+            operation = f"{num1} - {num2} = {result}"
+
         elif choice == '*':
-            print(mul(a, b))
+            result = multiply(num1, num2)
+            operation = f"{num1} * {num2} = {result}"
+
         elif choice == '/':
-            print(div(a, b))
+            result = divide(num1, num2)
+            operation = f"{num1} / {num2} = {result}"
+
         elif choice == '^':
-            print(power(a, b))
+            result = power(num1, num2)
+            operation = f"{num1} ^ {num2} = {result}"
+
         elif choice == '%':
-            print(rem(a, b))
-    except:
-        print("Something Went Wrong")
-       
-      
-    return True
-       
-      
+            result = remainder(num1, num2)
+            operation = f"{num1} % {num2} = {result}"
+
+        else:
+            print("Something Went Wrong")
+            return 0
+
+        print(operation)
+        history.append(operation)
+
+    else:
+        print("Unrecognized operation")
+
+    return 0
 
 
-#End the select_op(choice) function here
-#-------------------------------------
-#This is the main loop. It covers Task 1 (Section 1)
-#YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE
 while True:
-  print("Select operation.")
-  print("1.Add      : + ")
-  print("2.Subtract : - ")
-  print("3.Multiply : * ")
-  print("4.Divide   : / ")
-  print("5.Power    : ^ ")
-  print("6.Remainder: % ")
-  print("7.Terminate: # ")
-  print("8.Reset    : $ ")
-  
+    print("Select operation.")
+    print("1.Add      : + ")
+    print("2.Subtract : - ")
+    print("3.Multiply : * ")
+    print("4.Divide   : / ")
+    print("5.Power    : ^ ")
+    print("6.Remainder: % ")
+    print("7.Terminate: # ")
+    print("8.Reset    : $ ")
+    print("8.History  : ? ")
 
-  # take input from the user
-  choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
-  print(choice)
-  if(select_op(choice) == -1):
-    #program ends here
-    print("Done. Terminating")
-    exit()
+    choice = input("Enter choice(+,-,*,/,^,%,#,$,?): ")
+    print(choice)
+
+    if(select_op(choice) == -1):
+        print("Done. Terminating")
+        exit()
